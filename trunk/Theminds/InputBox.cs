f0e24cf007d5f653a6b4a84752144fde11c7604e
shadytrees <shadytrees@e851d657-9a19-0410-aec9-5111d4c6a560>
2006-12-03 23:20:18 +0000

    ZE BEEPING HAS STOPPED!

--- trunk/Theminds/InputBox.cs
+++ trunk/Theminds/InputBox.cs
@@ -15,32 +15,26 @@ namespace Theminds {
 		public event MessageDel Message;
 		public event StopPressesDel StopPresses;
 
-		protected override void OnKeyDown(KeyEventArgs e) {
-			// Press Esc, line turns empty. Similar to Command Prompt.
-			if (Keys.Escape == e.KeyCode) Text = null;
-			if (Keys.Enter != e.KeyCode) return;
+      protected override void OnKeyDown(KeyEventArgs e) {
+         e.SuppressKeyPress = true;
+         if (Keys.Escape == e.KeyCode) Text = null;
+         if (Keys.Enter != e.KeyCode) return;
 
-			bool shouldStop = false;
-			StopPresses(ref shouldStop);
-			if (shouldStop) return;
+         bool shouldStop = false;
+         StopPresses(ref shouldStop);
+         if (shouldStop) return;
 
-			if (Text.StartsWith("/")) {
-				int y = Text.IndexOf(' ');
+         if (Text.StartsWith("/")) {
+            int y = Text.IndexOf(' ');
 
-				// Check command for any parameters.
-				string command = (-1 != y) ? Text.Substring(1, y - 1) : Text.Substring(1);
-				string arg = (-1 != y) ? Text.Substring(y + 1) : "";
+            // Check for parameters.
+            string command = (-1 != y) ? Text.Substring(1, y - 1) : Text.Substring(1);
+            string arg = (-1 != y) ? Text.Substring(y + 1) : "";
 
-				Command(command, arg);
-
-				// TODO: Erase text once we implement command history.
-			}
-
-			// Precondition: Connection must be non-null.
-			else if (Text.Length > 0) Message(Text);
-
-			// Mandatory per Windows Forms docs.
-			base.OnKeyDown(e);
-		} // OnKeyDown
+            Command(command, arg);
+         }
+         else if (Text.Length > 0) Message(Text);
+         base.OnKeyDown(e);
+      }
 	}
 }
