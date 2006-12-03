efda95b6f0e08faed3d0613a3a9cad534caf3043
shadytrees <shadytrees@e851d657-9a19-0410-aec9-5111d4c6a560>
2006-12-03 15:52:28 +0000

    Lion.cs -> Ideas.cs; Ideas.TestContains(string, string[]) added and used in JPQ filters; post-build event to copy lion.txt added; TODOs added to Page.cs

--- Theminds/Page.cs
+++ Theminds/Page.cs
@@ -8,44 +8,49 @@ using System.Collections.Generic;
 using Aspirations;
 
 namespace Theminds {
-	sealed partial class Page : Form, IBuffer {
-		public static char[] Space = new char[] { ' ' };
-		Quirk connection;
+   sealed partial class Page : Form, IBuffer {
+      public static char[] Space = new char[] { ' ' };
+      Quirk connection;
 
       string currentChannel;
-		public string CurrentChannel {
-			get { return currentChannel; }
-			set { currentChannel = value; }
-		}
+      public string CurrentChannel {
+         get { return currentChannel; }
+         set { currentChannel = value; }
+      }
 
       public static Ideas Lion;
 
-		// Testcase: Join a channel, try messaging.
+      // Testcase: Join a channel, try messaging.
+      // TODO: handle tab close viv parting a channel.
+      // TODO: set up a preference system to remove hardcoded lion.txt
+      // TODO: fix button and user list font face
+      // TODO: depressed button state?
+      // TODO: disable beep
       Dictionary<string, LogBox> logBoxes;
       Dictionary<string, ITab> tabs;
       Dictionary<int, string> channelNames;
-		public Page() {
-			SetUp(); // MainForm.SetUpForm.cs
-			PostLine += delegate { };
+      public Page() {
+         SetUp(); // MainForm.SetUpForm.cs
+         PostLine += delegate { };
          Lion = new Ideas(@"lion.txt");
 
-			QuirkStart mozNet = new QuirkStart();
-				mozNet.nick = "Tongue"; mozNet.port = 6667;
-				mozNet.serv = "irc.mozilla.org";
-				mozNet.user = "USER cryptoliter2 8 * :Hi";
-			connection = new Quirk(mozNet);
-			connection.Line += new Quirk.LineDel(BufferLine);
+         QuirkStart mozNet = new QuirkStart();
+         mozNet.nick = "Tongue"; mozNet.port = 6667;
+         mozNet.serv = "irc.mozilla.org";
+         mozNet.user = "USER cryptoliter2 8 * :Hi";
+         connection = new Quirk(mozNet);
+         connection.Line += new Quirk.LineDel(BufferLine);
 
-			LogBoxFilters.Init(connection, this);
+         LogBoxFilters.Init(connection, this);
          JoinPartQuitFilter.Init(connection, this);
-			InputBoxFilters.Init(connection, inputBox, this);
+         InputBoxFilters.Init(connection, inputBox, this);
          InitBuffering();
 
-			// For StartSeedingUserList()
-			tmpUserListItems = new System.Collections.Generic.List<string>();
+         // For StartSeedingUserList()
+         tmpUserListItems = new System.Collections.Generic.List<string>();
 
          connection.Start();
-		}
+      }
 
       /**** Construction workers ****/
       void InitBuffering() {
@@ -63,27 +68,27 @@ namespace Theminds {
          tabber.Moved += new TabDel(MoveChannelTab);
       }
 
-		/**** Event handlers ****/
-		protected override void OnClosing(System.ComponentModel.CancelEventArgs e) {
-			connection.Dispose();
-			base.OnClosing(e);
-		}
+      /**** Event handlers ****/
+      protected override void OnClosing(System.ComponentModel.CancelEventArgs e) {
+         connection.Dispose();
+         base.OnClosing(e);
+      }
 
-		protected override void OnKeyDown(KeyEventArgs e) {
-			if (false == e.Control) goto noControl;
-			switch (e.KeyCode) {
-				case Keys.PageUp: tabber.MoveToPrev(); break;
-				case Keys.PageDown: tabber.MoveToNext(); break;
-				case Keys.T: tabber.Add(); break;
-				case Keys.W: tabber.RemoveCurrent(); break;
-				case Keys.Q: this.Close(); break;
-			}
+      protected override void OnKeyDown(KeyEventArgs e) {
+         if (false == e.Control) goto noControl;
+         switch (e.KeyCode) {
+            case Keys.PageUp: tabber.MoveToPrev(); break;
+            case Keys.PageDown: tabber.MoveToNext(); break;
+            case Keys.T: tabber.Add(); break;
+            case Keys.W: tabber.RemoveCurrent(); break;
+            case Keys.Q: this.Close(); break;
+         }
 
-		noControl:
-			base.OnKeyDown(e);
-		}
+      noControl:
+         base.OnKeyDown(e);
+      }
 
-		/**** Static members ****/
-		public static void Alert(object alert) { MessageBox.Show(alert.ToString()); }
-	}
+      /**** Static members ****/
+      public static void Alert(object alert) { MessageBox.Show(alert.ToString()); }
+   }
 }
