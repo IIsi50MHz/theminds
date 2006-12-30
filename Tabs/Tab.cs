735edaafe256c9560dc4b971b882fbc9a4db936c
shadytrees <shadytrees@e851d657-9a19-0410-aec9-5111d4c6a560>
2006-12-30 02:00:44 +0000

    Fixing weird tabbing issues

--- Tabs/Tab.cs
+++ Tabs/Tab.cs
@@ -5,34 +5,34 @@ using System.Drawing;
 using System.Diagnostics;
 
 namespace Aspirations {
-	public sealed class Tab : Button, ITab {
-		public double Shrinkage {
-			get { return shrinkage; }
-			set {
-				if (value == shrinkage) return;
-				this.shrinkage = value;
-				this.Width = (int) (this.trueWidth * value);
-			}
-		}
-		double shrinkage = 1.0;
+   public sealed class Tab : Button, ITab {
+      public double Shrinkage {
+         get { return shrinkage; }
+         set {
+            if (value == shrinkage) return;
+            this.shrinkage = value;
+            this.Width = (int)(this.trueWidth * value);
+         }
+      }
+      double shrinkage = 1.0;
 
-		int trueWidth;
+      int trueWidth;
 
-		// del: Add |this| to an object that will calculate its
-		//    autosized width.
-		public Tab(KissWidthDel del)
-			: base() {
-			this.AutoSize = true;
-			del(this);
-			this.trueWidth = this.Width;
-			this.AutoSize = false;
+      // del: Add |this| to an object that will calculate its
+      //    autosized width.
+      public Tab(KissWidthDel del)
+         : base() {
+         this.AutoSize = true;
+         del(this);
+         this.trueWidth = this.Width;
+         this.AutoSize = false;
 
-			this.Width = (int) (trueWidth * Shrinkage);
-		}
+         this.Width = (int)(trueWidth * Shrinkage);
+      }
 
       public Tab(KissWidthDel del, string text)
          : this(del) { this.Text = text; }
 
-		public int TrueWidth { get { return trueWidth; } }
-	}
+      public int TrueWidth { get { return trueWidth; } }
+   }
 }
