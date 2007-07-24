d9385baea4580c02f5392dca66caa0fa59d333c4
shadytrees <shadytrees@e851d657-9a19-0410-aec9-5111d4c6a560>
2007-07-24 18:14:40 +0000

    Ideas gains write ability

--- Structures/Ideas.cs
+++ Structures/Ideas.cs
@@ -1,16 +1,33 @@
+using System;
 using System.IO;
 using System.Diagnostics;
 using System.Collections.Specialized;
 
 namespace Aspirations {
-   public class Ideas {
+   public class Ideas : IDisposable {
       public string Get(string id) { return dict[id]; }
 
       public string Get(string id, string id2) {
          return Get(id + "." + id2);
       }
 
-      public Ideas(string file) {
+      public Ideas Set(string id, string value) {
+         dict[id] = value;
+         return this;
+      }
+
+      public Ideas Set(string id, string id2, string value) {
+         return Set(id, id2, value);
+      }
+
+      // Serves as the flag for Dispose() and for a read-only
+      // Ideas instance used, for example, on an internationalization
+      // file like lion.txt. A read/write example is a configuration
+      // file like config.txt.
+      bool trackChanges; string file;
+      public Ideas(string file, bool trackChanges) {
+         this.trackChanges = trackChanges;
+         this.file = file;
          dict = new StringDictionary();
          using (TextReader t = new StreamReader(file)) {
             string line;
@@ -18,8 +35,14 @@ namespace Aspirations {
          }
       }
 
+      public Ideas(string file) : this(file, false) { }
+
       /**** Private members ****/
       StringDictionary dict;
+
+      // [Ad hoc] The format of each line is two strings,
+      // each separated by a quotation mark. A simple
+      // string split and a check for \" suffices to parse.
       void parseLine(string line) {
          if (!line.StartsWith("\"")) return;
 
@@ -28,6 +51,18 @@ namespace Aspirations {
          catch (System.ArgumentException) {
             dict[tokens[1]] = tokens[3];
          }
+      }
+
+      public void Dispose() {
+         if (!trackChanges) return;
+         
+         using (TextWriter t = new StreamWriter(file)) {
+            foreach (string key in dict.Keys) {
+               t.WriteLine(String.Format("\"{0}\" \"{1}\"", key, dict[key]));
+            }
+         }
+
+         trackChanges = false;
       }
    } // class Ideas
 }
